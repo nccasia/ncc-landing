5a86c7f7ca3a0df5c0cb6ff1260cc4e6d5f7ddcf
evilpeanut234 <mummy2341999@gmail.com>
2022-08-09 00:25:34 +0700

    update service v2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
+var closeIcon = document.querySelector(".icon_close");
 
 menuIcon.onclick = function () {
-	sidebar.classList.toggle("small-sidebar");
-}
+  sidebar.classList.toggle("small-sidebar");
+};
+
+closeIcon.onclick = function () {
+  sidebar.classList.onclick("close-sidebar");
+};
