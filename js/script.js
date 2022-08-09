5cd078ac82e8d267babc47d6b0c057fc3fca6c27
TrungdoNCC <trung.doduc@ncc.asia>
2022-08-09 14:38:10 +0700

    team-screen

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
+var closeIcon = document.querySelector(".icon_close");
 
 menuIcon.onclick = function () {
-	sidebar.classList.toggle("small-sidebar");
+  sidebar.classList.toggle("small-sidebar");
+};
+
+closeIcon.onclick = function () {
+  sidebar.classList.onclick("close-sidebar");
+};
+
+function toggleItem(id1, id2, itemTitle, itemContent) {
+  document.getElementById(id1).classList.toggle("none");
+  document.getElementById(id2).classList.toggle("none");
+  document.getElementById(itemTitle).classList.toggle("focus");
+  document.getElementById(itemContent).classList.toggle("none");
 }
