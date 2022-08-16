9e71fe9a628ff1e2dd796e015071ba8fed15885d
TrungdoNCC <trung.doduc@ncc.asia>
2022-08-16 17:57:12 +0700

    responsive casestudy s

--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ function toggleItem(id1, id2, itemTitle, itemContent) {
   document.getElementById(itemTitle).classList.toggle("focus");
   document.getElementById(itemContent).classList.toggle("none");
 }
+
+function openPopup(id) {
+  var popup = document.getElementById(id);
+  popup.classList.add("show");
+}
+
+function closePopup(id) {
+  var popup = document.getElementById(id);
+  popup.classList.remove("show");
+}
+
+window.addEventListener("click", ({ target }) => {
+  const popup = [...document.getElementsByClassName("popuptext")];
+  const popups = [...document.getElementsByClassName("content-popup")];
+  const popupContain = target.closest(".content-popup");
+
+  const clickedOnClosedPopup =
+    popupContain && !popupContain.classList.contains("show");
+
+  popups.forEach((p) =>
+    popup.forEach((element) => {
+      element.classList.remove("show");
+      p.classList.remove("show");
+    })
+  );
+
+  if (clickedOnClosedPopup) {
+    popupContain.classList.add("show");
+  }
+});
