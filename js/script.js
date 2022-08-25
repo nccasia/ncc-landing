7537b8be63ee77f1803ec764f5817f5dd9cff4ec
Tra77 <phamthutra77200@gmail.com>
2022-08-25 13:31:48 +0700

    fix animation

--- js/script.js
+++ js/script.js
@@ -129,25 +129,6 @@ function visible(partial) {
 
 }
 
-$(window).scroll(function(){
-
-if(visible($('.count-digit')))
-  {
-    if($('.count-digit').hasClass('counter-loaded')) return;
-    $('.count-digit').addClass('counter-loaded');
-    
-$('.count-digit').each(function () {
-var $this = $(this);
-jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-  duration: 5000,
-  easing: 'swing',
-  step: function () {
-    $this.text(Math.ceil(this.Counter));
-  }
-});
-});
-  }
-})
 
 
 
